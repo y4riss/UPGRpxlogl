55059b2b7a92d7aea9355bcca0e7d4148c3f0172
joessir <lassiryyass@gmail.com>
2023-07-06 15:35:33 +0100

    quiz finished

--- app.js
+++ app.js
@@ -54,3 +54,118 @@ const quiz = [
         answer : 1
     }
 ]
+
+const questionNumberDiv = document.querySelector("span");
+const questionDiv = document.querySelector(".question");
+const finishDiv = document.querySelector(".finish");
+
+const choicesLabel = document.querySelectorAll("label");
+const choicesRadio = document.getElementsByName("radio");
+const startButton = document.querySelector(".start button");
+const quizBody = document.querySelector(".quiz-body");
+const nextButton = document.querySelector(".ok");
+const restartButton = document.querySelector(".restart");
+
+let i = 0; // current question
+let score = 0;
+
+startButton.addEventListener("click",(e)=>{
+
+    document.querySelector(".start").classList.add("hide");
+    quizBody.classList.remove("hide");
+    start();
+
+})
+
+
+const resetCheckBox = ()=>
+{
+    choicesRadio.forEach(choice => choice.checked = false);
+}
+const start = () => {
+
+
+        
+        nextButton.textContent = "Ok";
+        questionNumberDiv.textContent = `Question ${i + 1}:`;
+        questionDiv.textContent = quiz[i].question;
+        let j = 0;
+        choicesLabel.forEach(choice => {
+            choice.textContent = quiz[i].choices[j++];
+        })
+
+        nextButton.addEventListener("click",handleClick);
+
+}
+
+
+const handleClick = (e)=>{
+
+        if (e.target.textContent == "Ok")
+        {
+            // check if answer selected
+            let answer = null;
+
+            choicesRadio.forEach(choice => {
+                if (choice.checked) {
+                    answer = choice.id;
+                    return ;
+                }
+            })
+            if (!answer)
+            {
+                alert("Please answer by choosing one of the available options.");
+            }
+            else
+            {
+                if (answer == quiz[i].answer) score++;
+
+                for(let k = 0 ; k < choicesRadio.length ; k++)
+                {
+                    if (choicesRadio[k].id == quiz[i].answer) choicesLabel[k].innerHTML += ` <i class="fa-solid fa-check" style="color: #00c742;"></i>`;
+                    else if (choicesRadio[k].checked) choicesLabel[k].innerHTML += ` <i class="fa-solid fa-xmark" style="color: #ff3838;"></i>`;
+                }
+
+
+                if (i == quiz.length - 1)
+                {
+                    nextButton.textContent = "Finish";
+                }
+                else
+                {
+                    nextButton.textContent = "Next";
+                }
+            }
+
+            // check if answer correct
+
+            // check if this is the last question
+
+            // display next question or score
+        }
+        else if (e.target.textContent == "Next")
+        {
+            resetCheckBox();
+            i++;
+            start();
+
+        }
+        else if (e.target.textContent == "Finish")
+        {
+           finish();
+        }
+
+}
+
+const finish = ()=>{
+
+    quizBody.classList.add("hide");
+    finishDiv.classList.remove("hide");
+
+    finishDiv.querySelector("p").textContent = `Score : ${score}/${quiz.length}`
+
+    restartButton.addEventListener("click",()=>{
+        location.reload();
+    })
+
+}
