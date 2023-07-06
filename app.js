92da3becee22c9571ca9ae038030dda6137bbfcf
joessir <lassiryyass@gmail.com>
2023-07-06 14:14:28 +0100

    added basic css and quiz array of objects

---
+++ app.js
@@ -0,0 +1,56 @@
+
+
+const quiz = [
+
+    {
+        question : "Which programming language is often used for developing Android applications?",
+        choices : [
+            "Java",
+            "Python",
+            "C++",
+            "Ruby"
+        ],
+        answer : 0
+    },
+    {
+        question : "What does CSS stand for in web development?",
+        choices : [
+            "Computer Style Sheets",
+            "Cascading Style Sheets",
+            "Creative Style Sheets",
+            "Colorful Style Sheets"
+        ],
+        answer : 1
+    },
+    {
+      question : "Which data structure follows the Last-In-First-Out (LIFO) principle?",
+      choices : [
+        "Queue",
+        "Linked List",
+        "Stack",
+        "Array"
+      ] ,
+      answer : 2
+    },
+    {
+        question : "Which of the following sorting algorithms has the worst-case time complexity of O(n^2)?",
+        choices : [
+
+            "Merge Sort",
+            "Quick Sort",
+            "Insertion sort",
+            "Radix sort",
+        ],
+        answer : 2
+    },
+    {
+        question : `What does the term "scope" refer to in programming?`,
+        choices : [
+            "The physical location where a variable is stored in memory",
+            "The lifespan or visibility of a variable",
+            "The process of converting source code into machine code",
+            "The process of finding and fixing bugs in a program"
+        ],
+        answer : 1
+    }
+]
